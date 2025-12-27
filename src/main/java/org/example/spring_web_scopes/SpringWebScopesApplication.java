2d009589fc2b125ced6fe37a69e5b877bd560b16
Hsu Myat Htike <hsumyathtike2004@gmail.com>
2025-12-27 16:14:15 +0630

    Initial commit

---
+++ src/main/java/org/example/spring_web_scopes/SpringWebScopesApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_web_scopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebScopesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebScopesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spring_web_scopes/SpringWebScopesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_web_scopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebScopesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
